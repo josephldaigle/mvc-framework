b31560814d51bbf35a9f14c834581397727c1c11
joed <joed@gdn13937.gdn.peachnet.edu>
2017-04-05 17:19:58 -0400

    - implemented ContainerAwareInterface

--- src/Framework.php
+++ src/Framework.php
@@ -3,13 +3,25 @@
 namespace Gordon\MVC;
 
 use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Framework.
  *
  * @author Joseph Daigle
  */
-class Framework extends HttpKernel
+class Framework extends HttpKernel implements ContainerAwareInterface
 {
+    public $container;
     
+    /**
+     * Sets the container.
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->$container = $container;
+    }
+
 }
