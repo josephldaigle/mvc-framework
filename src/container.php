aa42f84147bbb711914c4655f977a450bdb0dd6b
joed <joed@gdn13937.gdn.peachnet.edu>
2017-04-05 19:49:01 -0400

    - DI container initialized

---
+++ src/container.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This is the DI Container for the gscweb/mvc-framework project.
+ */
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+
+/**
+ * Create the container.
+ */
+$container = new ContainerBuilder();
+
+/**
+ * Configure the container.
+ */
+$container->setParameter('app.name', 'Application Name');
+
+/**
+ * Return the contianer.
+ */
+$container->compile();
+
+//var_dump($container);
+return $container;
