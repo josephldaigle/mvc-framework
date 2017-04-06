6e114fb79098eca9484385c84f574dcbed739091
joed <joed@gdn13937.gdn.peachnet.edu>
2017-04-05 22:57:59 -0400

    - added PHPMailer service - added gscweb Framework service

--- src/container.php
+++ src/container.php
@@ -5,8 +5,7 @@
  */
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-
-
+use Symfony\Component\Yaml\Yaml;
 /**
  * Create the container.
  */
@@ -15,7 +14,23 @@ $container = new ContainerBuilder();
 /**
  * Configure the container.
  */
-$container->setParameter('app.name', 'Application Name');
+
+// Register mailer
+$container->register('gsc.mailer', PHPMailer::class)
+        ->addMethodCall('isSMTP')
+        ->addMethodCall('isHtml', array(true))
+        ->addMethodCall('setFrom', array('noreply@gordonstate.edu', 'Gordon State College'))
+        ->setProperty('Host', 'condor.gordonstate.edu');
+        
+$container->setParameter('gsc.app.config', Yaml::parse(file_get_contents(HOME_DIR . '/config/services/mailer.yml')));
+
+
+$container->register('gsc.framework', Gordon\MVC\Framework::class)
+        ->setArguments(array(
+            new Symfony\Component\EventDispatcher\EventDispatcher(),
+            new \Symfony\Component\HttpKernel\Controller\ControllerResolver()
+        ))
+        ->addMethodCall('setContainer', array($container));
 
 /**
  * Return the contianer.
