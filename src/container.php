ed0bcc0db03e34afc2f533c1ee92e13db77a676f
joed <joed@gdn13937.gdn.peachnet.edu>
2017-04-06 21:28:54 -0400

    - implement app, service and routing configurations - implement routing, and event handling services

--- src/container.php
+++ src/container.php
@@ -5,8 +5,12 @@
  */
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Loader\YamlFileLoader as RouteLoader;
+use Symfony\Component\Routing\RouteCollectionBuilder;
 use Symfony\Component\Config\FileLocator;
 
 /**
@@ -14,27 +18,86 @@ use Symfony\Component\Config\FileLocator;
  */
 $container = new ContainerBuilder();
 
-/**
- * Configure the container.
- */
-$loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/config'));
+/**********************
+ * Configuration
+ *********************/
+
+//create a file locator
+$locator = new FileLocator(HOME_DIR . '/config');
+$configLoader = new YamlFileLoader($container, $locator);
 
 //load application configuration
-$container->setParameter('gsc.config', Yaml::parse(file_get_contents(HOME_DIR . '/config/application.yml')));
+$container->setParameter('app.config', new ParameterBag(Yaml::parse(file_get_contents($locator->locate('application.yml')))));
 
-//load services
-$loader->load('services.yml');
+//load configured services
+$configLoader->import('services.yml');
+
+//resolve placeholders
+$container->resolveEnvPlaceholders('%app.config%');
 
 
+//register a request stack
+$container->register('gsc.request_stack', \Symfony\Component\HttpFoundation\RequestStack::class);
+$container->register('gsc.request_context', \Symfony\Component\Routing\RequestContext::class)
+        ->addMethodCall('fromRequest', array($request));
+
+
+
+/**********************
+ * Routing
+ *********************/
+//load routes
+$routeLoader = new RouteLoader($locator);
+$routes = new RouteCollectionBuilder($routeLoader);
+
+foreach (glob(HOME_DIR . '/config/routes/*.yml') as $route) {
+    $routes->import($route, $container->getParameterBag()->get('app.config')->get('app.name'));
+}
+$routes = $routes->build();
+
+$container->register("gsc.url_matcher", Symfony\Component\Routing\Matcher\UrlMatcher::class)
+        ->setArguments(array(
+            $routes, 
+            new Reference('gsc.request_context')
+        ));
+
+$container->register('gsc.router_listener', \Symfony\Component\HttpKernel\EventListener\RouterListener::class)
+        ->setArguments(array(
+            new Reference('gsc.url_matcher'),
+            new Reference('gsc.request_stack')
+        ));
+
+
+
+/**********************
+ * Controllers
+ *********************/
+$container->register('gsc.controller_resolver', \Symfony\Component\HttpKernel\Controller\ControllerResolver::class);
+
+$container->register('gsc.argument_resolver', Symfony\Component\HttpKernel\Controller\ArgumentResolver::class);
+
+
+
+/**********************
+ * Event Dispatcher
+ *********************/
+$container->register('gsc.event_dispatcher', Symfony\Component\EventDispatcher\EventDispatcher::class)
+        ->addMethodCall('addSubscriber', array(new Reference('gsc.router_listener')));
+
+//register the framework class
 $container->register('gsc.framework', Gordon\MVC\Framework::class)
         ->setArguments(array(
-            new Symfony\Component\EventDispatcher\EventDispatcher(),
-            new \Symfony\Component\HttpKernel\Controller\ControllerResolver()
+            new Reference('gsc.event_dispatcher'),
+            new Reference('gsc.controller_resolver')
         ))
         ->addMethodCall('setContainer', array($container));
 
-/**
- * Return the contianer.
- */
+
+
+
+
+/**********************
+ * Build
+ *********************/
 $container->compile();
 return $container;
